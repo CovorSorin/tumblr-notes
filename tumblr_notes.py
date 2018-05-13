5519d267728a1983eafee3d3827e6844c2988f43
sorincovor <sorincovor@yahoo.com>
2018-05-13 13:27:33 +0300

    get all the pages

--- tumblr_notes.py
+++ tumblr_notes.py
@@ -1,24 +1,40 @@
 from lxml import html
 import requests
 
-URL = 'http://madakikoeru.tumblr.com/archive'
+print('Counting...')
 
-page = requests.get(URL)
-tree = html.fromstring(page.content)
+URL = 'https://madakikoeru.tumblr.com/page/'
 
-notes = tree.xpath("//span[contains(@class, 'post_notes')]/text()")
-
+total_posts = 0
 total_notes = 0
 max_notes = 0
 
-for note in notes:
-    note = int(note.strip()[:-6])
-    total_notes += note
+reached_end = False
+current_page = 0
+
+while (not reached_end):
+    current_page += 1
+
+    page = requests.get(URL + str(current_page))
+    tree = html.fromstring(page.content)
+
+    # ignore the reblogged posts
+    notes = tree.xpath("//*[contains(@class, 'post') and not(contains(@class, 'reblogged'))]//li[contains(@class, 'notes')]/a/text()")
+
+    if (not notes):
+        reached_end = True
+        break
+
+    total_posts += len(notes)
+    for note in notes:
+        note = int(note)
+        total_notes += note
 
     if (max_notes < note):
         max_notes = note
 
-print('Number of posts:', len(notes))
+print('Number of pages:', current_page)
+print('Number of posts:', total_posts)
 print('Total notes:', total_notes)
 print('Max number of notes of one post:', max_notes)
-print('Average number of notes per post:', total_notes / len(notes))
+print('Average number of notes per post:', round(total_notes / total_posts))
