f525282c5c3d437fae8d09cf6b62ae41b2248696
sorincovor <sorincovor@yahoo.com>
2018-05-13 13:58:56 +0300

    ignore reblogs

--- tumblr_notes.py
+++ tumblr_notes.py
@@ -3,10 +3,13 @@ import requests
 
 print('Counting...')
 
-URL = 'https://madakikoeru.tumblr.com/page/'
+USERNAME = 'madakikoeru'
+URL = 'https://' + USERNAME + '.tumblr.com/page/'
 
 total_posts = 0
+total_reblogs = 0
 total_notes = 0
+total_reblog_notes = 0
 max_notes = 0
 
 reached_end = False
@@ -18,23 +21,28 @@ while (not reached_end):
     page = requests.get(URL + str(current_page))
     tree = html.fromstring(page.content)
 
-    # ignore the reblogged posts
-    notes = tree.xpath("//*[contains(@class, 'post') and not(contains(@class, 'reblogged'))]//li[contains(@class, 'notes')]/a/text()")
+    posts = tree.xpath("//*[contains(@class, 'post')]//li[contains(@class, 'notes')]/a/text()")
+    reblogs = tree.xpath("//*[contains(@class, 'reblogged')]//li[contains(@class, 'notes')]/a/text()")
 
-    if (not notes):
+    if (not posts):
         reached_end = True
         break
 
-    total_posts += len(notes)
-    for note in notes:
-        note = int(note)
-        total_notes += note
+    total_posts += len(posts)
+    for post in posts:
+        post = int(post)
+        total_notes += post
 
-    if (max_notes < note):
-        max_notes = note
+        if (max_notes < post):
+            max_notes = post
 
+    total_reblogs += len(reblogs)
+    for reblog in reblogs:
+        total_reblog_notes += int(reblog)
+
+print('Stats for ' + USERNAME + ':')
 print('Number of pages:', current_page)
-print('Number of posts:', total_posts)
-print('Total notes:', total_notes)
-print('Max number of notes of one post:', max_notes)
-print('Average number of notes per post:', round(total_notes / total_posts))
+print('Number of posts:', total_posts - total_reblogs, '/', total_posts, '(' + str(total_reblogs) + ' reblogs ignored))')
+print('Total notes:', total_notes - total_reblog_notes, '(ignoring', total_reblog_notes, 'reblogs)')
+print('Best post:', max_notes, 'notes')
+print('Average:', round(total_notes / total_posts), 'notes / post')
